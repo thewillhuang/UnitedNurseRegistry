d3b900198e6eca63233c454ed7e7aa2c35aa1f13
William Huang <will.h86@gmail.com>
2015-06-15 23:08:58 -0700

    added config file so it will load

---
+++ app/js/config.js
@@ -0,0 +1,9 @@
+'use strict';
+//internally with external api (for beta testing)
+// var GLOBAL_WEB_ROOT = "http://beta.lifespeed.io/";
+
+//local machine and external api (when working on updates)
+var GLOBAL_WEB_ROOT = "http://localhost:3000/";
+
+
+var API_ROOT = "http://api.lifespeed.io/lifespeed.api/";

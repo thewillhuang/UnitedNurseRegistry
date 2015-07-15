adea118fe36f364bf88e2e5c6331785433e32a9d
William Huang <will.h86@gmail.com>
2015-07-15 01:35:40 -0700

    make sourcemaps work with browserify and uglify and css

--- app/js/react/components/dashboardWidgetLoader.jsx
+++ app/js/react/components/dashboardWidgetLoader.jsx
@@ -3,7 +3,7 @@ const React = require('react');
 const CircleOfTrustListBox = require('./circleOfTrustListBox.jsx');
 const NotificationBell = require('./notificationBell.jsx');
 const MessengingBubble = require('./messageNotification.jsx');
-
+console.log('random test to see if sorcemaps work');
 React.render(<NotificationBell />, document.getElementById('reactNavItems'));
 
 React.render(<MessengingBubble />, document.getElementById('messenging'));

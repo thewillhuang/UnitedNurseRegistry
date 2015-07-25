b05c033bd4f885db6dfe075f1adcc5075abc90dd
William Huang <will.h86@gmail.com>
2015-07-25 10:14:31 -0700

    remove compression, json too small, and all static files are gzipped

--- server.js
+++ server.js
@@ -5,17 +5,16 @@ const send = require('koa-send');
 const logger = require('koa-logger');
 const etag = require('koa-etag');
 const build = '/public';
-const conditional = require('koa-conditional-get');
 const path = require('path');
 const app = koa();
 const koaBody = require('koa-better-body');
-const compress = require('koa-compress');
 // const passport = require('koa-passport');
 
 // logging
 app.use(logger());
 
 // returns status code 304 if etag is the same
+const conditional = require('koa-conditional-get');
 app.use(conditional());
 
 // enable proxy through another server
@@ -28,26 +27,23 @@ app.use(koaBody());
 // app.use(passport.initialize());
 // app.use(passport.session());
 
-// routers
+// unprotected routes
+
+// protected routes
 require('./server/routes/userRoutes.js')(app);
 
-// add etag for cacheing
+// add etag for cacheing static files
 app.use(etag());
 
 // static file server
 app.use(function* () {
-  let opts = {
-    root: path.join(__dirname, build)
-  };
+  let opts = { root: path.join(__dirname, build) };
   if (this.path === '/') {
     yield send(this, 'index.html', opts);
   } else {
     yield send(this, this.path, opts);
   }
 });
-
-// compression
-app.use(compress());
 
 app.listen(port);
 console.log('server listening on port:', port);

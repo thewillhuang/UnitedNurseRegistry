b27e67704a6a0914f4a4c76859be71d72701fe19
William Huang <will.h86@gmail.com>
2015-07-24 18:41:34 -0700

    added CURD for user email, phone, address

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -20,7 +20,7 @@ module.exports = function (app) {
       });
   })
 
-  //create new user email
+  //create new user email with user id
   .post('/email/:userID', function* () {
     let userID = this.params.userID;
     let requestJson = this.request.body.fields;
@@ -31,20 +31,68 @@ module.exports = function (app) {
       .catch(function (err) {
         console.log(err);
       });
-    let userEmailJson = { 'fk_UserEmail_userID': userID, 'fk_UserEmail_emailID': email.insertId };
+    let userEmailJson = {
+      'fk_UserEmail_userID': userID,
+      'fk_UserEmail_emailID': email.insertId
+    };
     let q2 = {};
     q2.sql = 'INSERT INTO ?? SET ?';
-    q2.values = [
-      'useremail',
-      userEmailJson
-    ];
+    q2.values = ['useremail', userEmailJson];
     this.body = yield client.query(q2)
       .catch(function (err) {
         console.log(err);
       });
   })
 
-  //grab user table info based on userid
+  //create new user phone with user id
+  .post('/phone/:userID', function* () {
+    let userID = this.params.userID;
+    let requestJson = this.request.body.fields;
+    let q = {};
+    q.sql = 'INSERT INTO ?? SET ?';
+    q.values = ['phone', requestJson];
+    let phone = yield client.query(q)
+      .catch(function (err) {
+        console.log(err);
+      });
+    let userPhoneJson = {
+      'fk_UserPhone_userID': userID,
+      'fk_UserPhone_phoneID': phone.insertId
+    };
+    let q2 = {};
+    q2.sql = 'INSERT INTO ?? SET ?';
+    q2.values = ['userphone', userPhoneJson];
+    this.body = yield client.query(q2)
+      .catch(function (err) {
+        console.log(err);
+      });
+  })
+
+  //create new user address with user id
+  .post('/address/:userID', function* () {
+    let userID = this.params.userID;
+    let requestJson = this.request.body.fields;
+    let q = {};
+    q.sql = 'INSERT INTO ?? SET ?';
+    q.values = ['address', requestJson];
+    let address = yield client.query(q)
+      .catch(function (err) {
+        console.log(err);
+      });
+    let userAddressJson = {
+      'fk_UserAddress_userID': userID,
+      'fk_UserAddress_phoneID': address.insertId
+    };
+    let q2 = {};
+    q2.sql = 'INSERT INTO ?? SET ?';
+    q2.values = ['Address', userAddressJson];
+    this.body = yield client.query(q2)
+      .catch(function (err) {
+        console.log(err);
+      });
+  })
+
+  //grab user table info based on user id
   .get('/:userID', function* () {
     let userID = this.params.userID;
     let q = {};
@@ -56,7 +104,7 @@ module.exports = function (app) {
       });
   })
 
-  //grab user emails based on userid
+  //grab user emails based on user id
   .get('/email/:userID', function* () {
     let userID = this.params.userID;
     let q = {};
@@ -68,7 +116,31 @@ module.exports = function (app) {
       });
   })
 
-  // update user data
+  //grab user phone based on user id
+  .get('/phone/:userID', function* () {
+    let userID = this.params.userID;
+    let q = {};
+    q.sql = 'select ?? from ?? ?? join ?? ?? on (?? = ??) where ?? = ?';
+    q.values = ['p.*', 'phone', 'p', 'userphone', 'up', 'up.fk_UserPhone_phoneID', 'p.phoneID', 'ue.fk_UserPhone_userID', userID];
+    this.body = yield client.query(q)
+      .catch(function (err) {
+        console.log(err);
+      });
+  })
+
+  //grab user address based on user id
+  .get('/address/:userID', function* () {
+    let userID = this.params.userID;
+    let q = {};
+    q.sql = 'select ?? from ?? ?? join ?? ?? on (?? = ??) where ?? = ?';
+    q.values = ['a.*', 'address', 'a', 'useraddress', 'ua', 'ua.fk_UserAddress_addressID', 'a.phoneID', 'ua.fk_UserAddress_userID', userID];
+    this.body = yield client.query(q)
+      .catch(function (err) {
+        console.log(err);
+      });
+  })
+
+  // update user data by user id
   .put('/:userID', function* () {
     let requestJson = this.request.body.fields;
     let userID = this.params.userID;
@@ -81,7 +153,7 @@ module.exports = function (app) {
       });
   })
 
-  // update user email based on emailID
+  // update user email based on email ID
   .put('/email/:emailID', function* () {
     let requestJson = this.request.body.fields;
     let emailID = this.params.emailID;
@@ -94,7 +166,33 @@ module.exports = function (app) {
       });
   })
 
-  // delete user by id
+  // update user phone based on phone ID
+  .put('/phone/:phoneID', function* () {
+    let requestJson = this.request.body.fields;
+    let phoneID = this.params.phoneID;
+    let q = {};
+    q.sql = 'UPDATE ?? SET ? WHERE ?? = ?';
+    q.values = ['phone', requestJson, 'phoneID', phoneID];
+    this.body = yield client.query(q)
+      .catch(function (err) {
+        console.log(err);
+      });
+  })
+
+  // update user address based on address ID
+  .put('/address/:addressID', function* () {
+    let requestJson = this.request.body.fields;
+    let addressID = this.params.addressID;
+    let q = {};
+    q.sql = 'UPDATE ?? SET ? WHERE ?? = ?';
+    q.values = ['address', requestJson, 'addressID', addressID];
+    this.body = yield client.query(q)
+      .catch(function (err) {
+        console.log(err);
+      });
+  })
+
+  // delete user by user id
   .delete('/:userID', function* () {
     let userID = this.params.userID;
     let q = {};
@@ -106,7 +204,7 @@ module.exports = function (app) {
       });
   })
 
-  // delete useremail by emailid
+  // delete useremail by email id
   .delete('/email/:emailID', function* () {
     let emailID = this.params.emailID;
     let q = {};
@@ -116,7 +214,32 @@ module.exports = function (app) {
       .catch(function (err) {
         console.log(err);
       });
+  })
+
+  // delete phone by phone id
+  .delete('/phone/:phoneID', function* () {
+    let phoneID = this.params.phoneID;
+    let q = {};
+    q.sql = 'DELETE FROM ?? WHERE ?? = ?';
+    q.values = ['phone', 'phoneID', phoneID];
+    this.body = yield client.query(q)
+      .catch(function (err) {
+        console.log(err);
+      });
+  })
+
+  // delete address by address id
+  .delete('/address/:addressID', function* () {
+    let addressID = this.params.addressID;
+    let q = {};
+    q.sql = 'DELETE FROM ?? WHERE ?? = ?';
+    q.values = ['address', 'addressID', addressID];
+    this.body = yield client.query(q)
+      .catch(function (err) {
+        console.log(err);
+      });
   });
 
-  app.use(user.routes()).use(user.allowedMethods());
+  app.use(user.routes())
+    .use(user.allowedMethods());
 };

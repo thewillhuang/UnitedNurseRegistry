614b66c0141dc7f22abf974dafe3efbc67c978bc
William Huang <will.h86@gmail.com>
2015-07-25 12:06:21 -0700

    post and get routes for user license, work history, schedule, specialty. eslint allow for non camel case sql statements, schema modifications

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -19,6 +19,58 @@ module.exports = function (app) {
     this.body = yield query(q);
   })
 
+  //create user license given user id
+  .post('/license/:userID', function* () {
+    let userID = this.params.userID;
+    let requestJson = this.request.body.fields;
+    let q = {};
+    requestJson.fk_UserLicense_userID = userID;
+    q.sql = 'INSERT INTO ?? SET ?';
+    q.values = ['UserLicense', requestJson];
+    this.body = yield query(q);
+  })
+
+  //create user schedule given user id
+  .post('/UserSchedule/:userID', function* () {
+    let userID = this.params.userID;
+    let requestJson = this.request.body.fields;
+    let q = {};
+    requestJson.fk_UserSchedule_userID = userID;
+    q.sql = 'INSERT INTO ?? SET ?';
+    q.values = ['UserSchedule', requestJson];
+    this.body = yield query(q);
+  })
+
+  //create user schedule given user id
+  .post('/UserWorkHistory/:userID/:facilityID', function* () {
+    let userID = this.params.userID;
+    let facilityID = this.params.facilityID;
+    let requestJson = this.request.body.fields;
+    let q = {};
+    requestJson.fk_UserSchedule_userID = userID;
+    requestJson.fk_UserWorkHistory_facilityID = facilityID;
+    q.sql = 'INSERT INTO ?? SET ?';
+    q.values = ['UserSchedule', requestJson];
+    this.body = yield query(q);
+  })
+
+  //create new user specialty with user id
+  .post('/specialty/:userID', function* () {
+    let userID = this.params.userID;
+    let requestJson = this.request.body.fields;
+    let q = {};
+    q.sql = 'INSERT INTO ?? SET ?;';
+    q.values = ['specialty', requestJson];
+    let specialty = yield query(q);
+    let userSpecialty = {};
+    userSpecialty.fk_UserSpecialty_userID = userID;
+    userSpecialty.fk_UserSpecialty_specialtyID = specialty.insertId;
+    let q2 = {};
+    q2.sql = 'INSERT INTO ?? SET ?;';
+    q2.values = ['UserSpecialty', userSpecialty];
+    this.body = yield query(q2);
+  })
+
   //create new user email with user id
   .post('/email/:userID', function* () {
     let userID = this.params.userID;
@@ -27,13 +79,12 @@ module.exports = function (app) {
     q.sql = 'INSERT INTO ?? SET ?;';
     q.values = ['email', requestJson];
     let email = yield query(q);
-    let userEmailJson = {
-      'fk_UserEmail_userID': userID,
-      'fk_UserEmail_emailID': email.insertId
-    };
+    let userEmail = {};
+    userEmail.fk_UserEmail_userID = userID;
+    userEmail.fk_UserEmail_emailID = email.insertId;
     let q2 = {};
     q2.sql = 'INSERT INTO ?? SET ?;';
-    q2.values = ['useremail', userEmailJson];
+    q2.values = ['useremail', userEmail];
     this.body = yield query(q2);
   })
 
@@ -45,13 +96,12 @@ module.exports = function (app) {
     q.sql = 'INSERT INTO ?? SET ?';
     q.values = ['phone', requestJson];
     let phone = yield query(q);
-    let userPhoneJson = {
-      'fk_UserPhone_userID': userID,
-      'fk_UserPhone_phoneID': phone.insertId
-    };
+    let userPhone = {};
+    userPhone.fk_UserPhone_userID = userID;
+    userPhone.fk_UserPhone_phoneID = phone.insertId;
     let q2 = {};
     q2.sql = 'INSERT INTO ?? SET ?';
-    q2.values = ['userphone', userPhoneJson];
+    q2.values = ['userphone', userPhone];
     this.body = yield query(q2);
   })
 
@@ -63,13 +113,12 @@ module.exports = function (app) {
     q.sql = 'INSERT INTO ?? SET ?';
     q.values = ['address', requestJson];
     let address = yield query(q);
-    let userAddressJson = {
-      'fk_UserAddress_userID': userID,
-      'fk_UserAddress_phoneID': address.insertId
-    };
+    let userAddress = {};
+    userAddress.fk_UserAddress_userID = userID;
+    userAddress.fk_UserAddress_phoneID = address.insertId;
     let q2 = {};
     q2.sql = 'INSERT INTO ?? SET ?';
-    q2.values = ['Address', userAddressJson];
+    q2.values = ['Address', userAddress];
     this.body = yield query(q2);
   })
 
@@ -84,11 +133,50 @@ module.exports = function (app) {
     this.body = yield query(q);
   })
 
+  //grab user table info based on user id
+  .get('/UserSchedule/:userID', function* () {
+    let userID = this.params.userID;
+    let q = {};
+    q.sql = 'SELECT ??, ??, ?? FROM ?? WHERE ?? = ?';
+    q.values = ['shiftStart', 'shiftDuration', 'dayOfWeek', 'UserSchedule', 'fk_UserSchedule_userID', userID];
+    this.body = yield query(q);
+  })
+
+  //grab user table info based on user id
+  .get('/UserLicense/:userID', function* () {
+    let userID = this.params.userID;
+    let q = {};
+    q.sql = 'SELECT ??, ??, ?? FROM ?? WHERE ?? = ?';
+    q.values = ['licenseNumber', 'licenseState', 'licensePhotoUrl', 'UserLicense', 'fk_UserLicense_userID', userID];
+    this.body = yield query(q);
+  })
+
+  //grab user specialty based on user id
+  .get('/specialty/:userID', function* () {
+    let userID = this.params.userID;
+    let q = {};
+    q.sql = 'SELECT ?? FROM ?? AS ?? INNER JOIN ?? AS ?? on (?? = ??) WHERE ?? = ?';
+    q.values = ['s.specialty', 'specialty', 's', 'UserSpecialty', 'us', 'us.fk_UserSpecialty_specialtyID', 's.specialtyID', 'us.fk_UserSpecialty_userID', userID];
+    this.body = yield query(q);
+  })
+
+  //grab user work history based on user id
+  .get('/UserWorkHistory/:userID', function* () {
+    let userID = this.params.userID;
+    let q = {};
+    q.sql = 'SELECT ??, ??, ??, ?? FROM ?? AS ?? INNER JOIN ?? AS ?? on (?? = ??) WHERE ?? = ?';
+    let select = ['f.facilityID', 'f.facilityName', 'uwh.months', 'uwh.referenceName', 'uwh.referencePhone'];
+    let on = ['uwh.fk_UserWorkHistory_facilityID', 'f.facilityID'];
+    let where = ['us.fk_UserWorkHistory_userID', userID];
+    q.values = [select, 'UserWorkHistory', 'uwh', 'Facility', 'f', on, where];
+    this.body = yield query(q);
+  })
+
   //grab user emails based on user id
   .get('/email/:userID', function* () {
     let userID = this.params.userID;
     let q = {};
-    q.sql = 'SELECT ?? FROM ?? AS ?? JOIN ?? AS ?? on (?? = ??) WHERE ?? = ?';
+    q.sql = 'SELECT ?? FROM ?? AS ?? INNER JOIN ?? AS ?? on (?? = ??) WHERE ?? = ?';
     q.values = ['e.emailAddress', 'email', 'e', 'useremail', 'ue', 'ue.fk_UserEmail_emailID', 'e.emailID', 'ue.fk_UserEmail_userID', userID];
     this.body = yield query(q);
   })
@@ -97,7 +185,7 @@ module.exports = function (app) {
   .get('/phone/:userID', function* () {
     let userID = this.params.userID;
     let q = {};
-    q.sql = 'SELECT ?? FROM ?? AS ?? JOIN ?? AS ?? on (?? = ??) WHERE ?? = ?';
+    q.sql = 'SELECT ?? FROM ?? AS ?? INNER JOIN ?? AS ?? on (?? = ??) WHERE ?? = ?';
     q.values = ['p.*', 'phone', 'p', 'userphone', 'up', 'up.fk_UserPhone_phoneID', 'p.phoneID', 'ue.fk_UserPhone_userID', userID];
     this.body = yield query(q);
   })
@@ -106,7 +194,7 @@ module.exports = function (app) {
   .get('/address/:userID', function* () {
     let userID = this.params.userID;
     let q = {};
-    q.sql = 'select ?? from ?? ?? join ?? ?? on (?? = ??) where ?? = ?';
+    q.sql = 'SELECT ?? FROM ?? ?? INNER JOIN ?? ?? ON (?? = ??) WHERE ?? = ?';
     q.values = ['a.*', 'address', 'a', 'useraddress', 'ua', 'ua.fk_UserAddress_addressID', 'a.phoneID', 'ua.fk_UserAddress_userID', userID];
     this.body = yield query(q);
   })

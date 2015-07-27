15598fbdaf6dab71da1b8d0f50c7d0d289460977
William Huang <will.h86@gmail.com>
2015-07-26 21:52:04 -0700

    remove unused code in server

--- server.js
+++ server.js
@@ -35,14 +35,6 @@ app.use(compression());
 
 // protected routes
 require('./server/routes/userRoutes')(app);
-// require('./server/routes/user')(app);
-// require('./server/routes/userAddress')(app);
-// require('./server/routes/userEmail')(app);
-// require('./server/routes/userLicense')(app);
-// require('./server/routes/userPhone')(app);
-// require('./server/routes/userSchedule')(app);
-// require('./server/routes/userSpecialty')(app);
-// require('./server/routes/userWorkHistory')(app);
 
 // static file server
 var staticServer = function* () {

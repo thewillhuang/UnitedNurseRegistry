404754872d070c3033f21ea2526c3b87519f7e7d
William Huang <will.h86@gmail.com>
2015-07-28 16:29:06 -0700

    harmony for mocha

--- server/services/transactionStart.js
+++ server/services/transactionStart.js
@@ -1,31 +1,33 @@
 'use strict';
 const pool = require('./dbConnection');
 // a function that takes mysql parameters
-const transaction = function(q, q2, primaryKeyColumn) {
+const transaction = function(q, q2, setColumns) {
   // makes a db connection for a pool.
   return pool.getConnectionAsync().then(function(connection){
     return connection.beginTransactionAsync().then(function(){
       // start of q1
-      let result = connection.mysqlAsync(q).spread(function(rows){
-        return rows;
+      let result = connection.mysqlAsync(q).spread(function(rows, fields){
+        return {rows, fields};
       }).catch(function(err){
         console.log(err);
         return connection.rollback();
       });
 
+      let id = result.rows.insertId;
       // add the insertId of the previous transaction to the specified column.
-      q2[primaryKeyColumn] = result.insertId;
+      q2.values.push(setColumns);
 
       //start of q2
-      return connection.mysqlAsync(q2).spread(function(rows){
+      return connection.mysqlAsync(q2).spread(function(rows, fields){
         //commit the changes and release the connection on mysql success
         connection.commit();
         connection.release();
-        return rows;
+        return {rows, fields};
       }).catch(function(err){
         console.log(err);
         return connection.rollback();
       });
+
     });
   });
 };

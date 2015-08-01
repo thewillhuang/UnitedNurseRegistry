45c3d7ca536c5ab05f62edba794d33c3736d9141
William Huang <will.h86@gmail.com>
2015-07-31 19:50:00 -0700

    added workhistory routes and work history tests

--- server/routes/userWorkHistory.js
+++ server/routes/userWorkHistory.js
@@ -9,31 +9,32 @@ const query = require('../services/query');
 module.exports = function (app) {
   userWorkHistory
 
-  //create user schedule given user id
-  .post('/:userID/:facilityID', function* () {
+  //create user work history given user id and facility id
+  .post('/user/:userID/facility/:facilityID', function* () {
     let userID = this.params.userID;
     let facilityID = this.params.facilityID;
     let requestJson = this.request.body.fields;
     let q = {};
-    requestJson.fk_UserSchedule_userID = userID;
+    requestJson.fk_UserWorkHistory_userID = userID;
     requestJson.fk_UserWorkHistory_facilityID = facilityID;
     q.sql = 'INSERT INTO ?? SET ?';
-    q.values = ['UserSchedule', requestJson];
+    q.values = ['userworkhistory', requestJson];
+    // console.log(q);
     this.body = yield query(q);
   })
 
   //grab user work history based on user id
-  .get('/:userID', function* () {
+  .get('/user/:userID', function* () {
     let userID = this.params.userID;
     let q = {};
     q.sql = 'SELECT ?? FROM ?? AS ?? INNER JOIN ?? AS ?? on (?? = ??) WHERE ?? = ?';
     let select = ['uwh.userHistoryID', 'f.facilityID', 'f.facilityName', 'uwh.months', 'uwh.referenceName', 'uwh.referencePhone'];
-    q.values = [select, 'UserWorkHistory', 'uwh', 'Facility', 'f', 'uwh.fk_UserWorkHistory_facilityID', 'f.facilityID', 'us.fk_UserWorkHistory_userID', userID];
+    q.values = [select, 'UserWorkHistory', 'uwh', 'Facility', 'f', 'uwh.fk_UserWorkHistory_facilityID', 'f.facilityID', 'uwh.fk_UserWorkHistory_userID', userID];
     this.body = yield query(q);
   })
 
   // update user email based on email ID
-  .put('/:userHistoryID', function* () {
+  .put('/history/:userHistoryID', function* () {
     let requestJson = this.request.body.fields;
     let userHistoryID = this.params.userHistoryID;
     let q = {};
@@ -43,7 +44,7 @@ module.exports = function (app) {
   })
 
   // delete user specialty by user id and specialty id
-  .delete('/:userHistoryID', function* () {
+  .delete('/history/:userHistoryID', function* () {
     let userHistoryID = this.params.userHistoryID;
     let q = {};
     q.sql = 'DELETE FROM ?? WHERE ?? = ?';
--- test/userWorkHistory.js
+++ test/userWorkHistory.js
@@ -1,36 +1,276 @@
-// 'use strict';
-//
-// const chai = require('chai');
-// const expect = chai.expect;
-// const chaiAsPromised = require('chai-as-promised');
-// const supertest = require('supertest');
-// const app = require('../server');
-// const request = supertest(app.listen());
-// chai.use(chaiAsPromised);
-//
-// describe('user api', function () {
-//
-//   it('should reject invalid get requests', function (done) {
-//     request.get('/lolol')
-//       .expect(404)
-//       .end(function (err, res) {
-//         expect(res.body).to.be.an('object');
-//         expect(err).to.be.a('null');
-//         done();
-//       });
-//   });
-//
-//   it('should respond with empty array with unknown user', function (done) {
-//     request.get('/api/user/1')
-//       .expect(200)
-//       .end(function (err, res) {
-//         // console.log(res.body);
-//         expect(res.body).to.be.an('object');
-//         expect(res.body.rows).to.be.empty;
-//         expect(res.body.rows).to.be.an('array');
-//         expect(res.body.fields).to.be.an('array');
-//         expect(err).to.be.a('null');
-//         done();
-//       });
-//   });
-// });
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+var chaiAsPromised = require('chai-as-promised');
+var supertest = require('supertest');
+var app = require('../server');
+var request = supertest(app.listen());
+var uuid = require('node-uuid');
+chai.use(chaiAsPromised);
+
+describe('user schedule api', function () {
+
+  it('should reject invalid get requests', function (done) {
+    request.get('/api/userworkhistory/user/')
+      .expect(404)
+      .end(function (err, res) {
+        expect(res.body).to.be.an('object');
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  it('should respond with empty array with unknown user', function (done) {
+    request.get('/api/userworkhistory/user/abc')
+      .expect(200)
+      .end(function (err, res) {
+        // console.log(res.body);
+        expect(res.body).to.be.an('object');
+        expect(res.body.rows).to.be.an('array');
+        expect(res.body.rows).to.be.empty;
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  var r1;
+  it('should create a user', function (done) {
+    request.post('/api/user')
+      .send({
+        firstName: 'william',
+        lastName: 'huang',
+        middleName: 'w',
+        userGeoHash: 27898503349316,
+        userPwHash: '$2a$10$0vm3IMzEqCJwDwGNQzJYxOznt7kjXELjLOpOUcC7BjYTTEEksuhqy',
+        dob: '1986-04-08',
+        userName: uuid.v4()
+      })
+      .expect(200)
+      .end(function (err, res) {
+        r1 = res.body.rows;
+        expect(r1).to.be.an('object');
+        expect(r1.insertId).to.be.an('number');
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  let f1;
+  it('should insert a new facility given a correct object', function (done) {
+    request.post('/api/facility/')
+      .send({
+        facilityName: uuid.v4(),
+        facilityGeoHash: 27898503349316,
+        facilityPwHash: uuid.v4(),
+        facilityEMR: uuid.v4()
+      })
+      .expect(200)
+      .end(function (err, res) {
+        console.log(res.body);
+        f1 = res.body.rows;
+        expect(f1).to.be.an('object');
+        expect(f1.insertId).to.be.an('number');
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  it('insert work history 1 given a user id', function (done) {
+    request.post('/api/userworkhistory/user/' + r1.insertId + '/facility/' + f1.insertId)
+      .send({
+        months: 1,
+        referenceName: uuid.v4(),
+        referencePhone: 1234123141
+      })
+      .expect(200)
+      .end(function (err, res) {
+        // console.log(res.body);
+        expect(res).to.be.an('object');
+        expect(res.body.rows.insertId).to.be.an('number');
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  it('insert work history 2 given a user id', function (done) {
+    request.post('/api/userworkhistory/user/' + r1.insertId + '/facility/' + f1.insertId)
+      .send({
+        months: 3,
+        referenceName: uuid.v4(),
+        referencePhone: 123123412412
+      })
+      .expect(200)
+      .end(function (err, res) {
+        expect(res).to.be.an('object');
+        expect(res.body.rows.insertId).to.be.an('number');
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  var a1;
+  it('should have 2 schedule given a user id', function (done) {
+    request.get('/api/userworkhistory/user/' + r1.insertId)
+      .expect(200)
+      .end(function (err, res) {
+        console.log(res.body.rows);
+        expect(res.body).to.be.an('object');
+        expect(res.body.rows).to.be.not.empty;
+        expect(res.body.rows).to.be.an('array');
+        expect(res.body.rows).to.have.length(2);
+        a1 = res.body.rows[0].userHistoryID;
+        expect(res.body.fields).to.be.an('array');
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  it('should 200 given same data', function (done) {
+    request.get('/api/userworkhistory/user/' + r1.insertId)
+      .expect(200)
+      .end(function (err, res) {
+        // console.log(res.body.rows);
+        expect(res.body).to.be.an('object');
+        expect(res.body.rows).to.be.not.empty;
+        expect(res.body.rows).to.be.an('array');
+        expect(res.body.rows).to.have.length(2);
+        a1 = res.body.rows[0].userHistoryID;
+        expect(res.body.fields).to.be.an('array');
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  it('should delete an user schedule given an schedule ID', function (done) {
+    request.delete('/api/userworkhistory/history/' + a1)
+      .expect(200)
+      .end(function (err, res) {
+        expect(res.body.rows.affectedRows).to.equal(1);
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  var a2;
+  it('should have 1 schedule instead of 2', function (done) {
+    request.get('/api/userworkhistory/user/' + r1.insertId)
+      .expect(200)
+      .end(function (err, res) {
+        a2 = res.body.rows[0].userHistoryID;
+        expect(res.body).to.be.an('object');
+        expect(res.body.rows).to.be.not.empty;
+        expect(res.body.rows).to.be.an('array');
+        expect(res.body.rows).to.have.length(1);
+        expect(res.body.fields).to.be.an('array');
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  var newWorkHistory =  uuid.v4();
+  it('should update a schedule given an schedule id', function (done) {
+    request.put('/api/userworkhistory/history/' + a2)
+      .send({
+        months: 3,
+        referenceName: newWorkHistory,
+        referencePhone: 123123412412
+      })
+      .expect(200)
+      .end(function (err, res) {
+        // console.log(res.body);
+        expect(res.body).to.be.an('object');
+        expect(res.body.rows).to.be.not.empty;
+        expect(res.body.rows).to.be.an('object');
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  it('should have an updated schedule', function (done) {
+    request.get('/api/userworkhistory/user/' + r1.insertId)
+      .expect(200)
+      .end(function (err, res) {
+        expect(res.body).to.be.an('object');
+        expect(res.body.rows).to.be.not.empty;
+        expect(res.body.rows).to.be.an('array');
+        expect(res.body.rows).to.have.length(1);
+        expect(res.body.rows).to.have.deep.property('[0].referenceName', newWorkHistory);
+        expect(res.body.fields).to.be.an('array');
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  it('should delete user schedule 1 given an schedule ID', function (done) {
+    request.delete('/api/userworkhistory/history/' + a2)
+      .expect(200)
+      .end(function (err, res) {
+        expect(res.body.rows.affectedRows).to.equal(1);
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  it('should have 0 phone number instead of 1', function (done) {
+    request.get('/api/userworkhistory/user/' + r1.insertId)
+      .expect(200)
+      .end(function (err, res) {
+        // console.log(res.body);
+        expect(res.body).to.be.an('object');
+        expect(res.body.rows).to.be.empty;
+        expect(res.body.rows).to.be.an('array');
+        expect(res.body.rows).to.have.length(0);
+        expect(res.body.fields).to.be.an('array');
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  it('should delete a user given a correct user id', function (done) {
+    request.delete('/api/user/' + r1.insertId)
+      .expect(200)
+      .end(function (err, res) {
+        expect(res.body.rows.affectedRows).to.equal(1);
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  it('should delete a facility given a correct facility id', function (done) {
+    request.delete('/api/facility/' + f1.insertId)
+      .expect(200)
+      .end(function (err, res) {
+        expect(res.body.rows.affectedRows).to.equal(1);
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  it('the deleted facility should not exist', function (done) {
+    request.get('/api/facility/' + f1.insertId)
+      .expect(200)
+      .end(function (err, res) {
+        expect(res.body).to.be.an('object');
+        expect(res.body.rows).to.be.empty;
+        expect(res.body.rows).to.be.an('array');
+        expect(res.body.fields).to.be.an('array');
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+  it('the deleted user should not exist', function (done) {
+    request.get('/api/user/' + r1.insertId)
+      .expect(200)
+      .end(function (err, res) {
+        expect(res.body).to.be.an('object');
+        expect(res.body.rows).to.be.empty;
+        expect(res.body.rows).to.be.an('array');
+        expect(res.body.fields).to.be.an('array');
+        expect(err).to.be.a('null');
+        done();
+      });
+  });
+
+});

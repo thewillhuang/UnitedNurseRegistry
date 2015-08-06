4616dcd18a9578952ef80b4e8e2c95919e8f5f81
William Huang <will.h86@gmail.com>
2015-08-06 00:10:24 -0700

    added shiftReview routes for both facility and userID

---
+++ server/routes/shiftReviews.js
@@ -0,0 +1,110 @@
+'use strict';
+
+const Router = require('koa-router');
+const shiftReview = new Router({
+  prefix: '/api/shiftReview',
+});
+const query = require('../services/query');
+
+module.exports = function shiftReviewRoutes(app) {
+  shiftReview
+
+  .post('/user/:userID/shift/:shiftID', function* createUserReview() {
+    const userID = this.params.userID;
+    const shiftID = this.params.shiftID;
+    const requestJson = this.request.body.fields;
+    requestJson.fk_ShiftReviewOnUser_userID = userID;
+    requestJson.fk_ShiftReviewOnUser_shiftID = shiftID;
+    const q = {};
+    q.sql = 'INSERT INTO ?? SET ?';
+    q.values = ['ShiftReviewOnUser', requestJson];
+    this.body = yield query(q);
+  })
+
+  .put('/user/:userID/shift/:shiftID', function* updateUserReview() {
+    const userID = this.params.userID;
+    const shiftID = this.params.shiftID;
+    const requestJson = this.request.body.fields;
+    const q = {};
+    q.sql = 'UPDATE ?? SET ? WHERE ?? = ? AND ?? = ?';
+    q.values = ['ShiftReviewOnUser', requestJson, 'fk_ShiftReviewOnUser_userID', userID, 'fk_ShiftReviewOnUser_shiftID', shiftID];
+    this.body = yield query(q);
+  })
+
+  .get('/user/:userID/shift/:shiftID', function* getUserReview() {
+    const userID = this.params.userID;
+    const shiftID = this.params.shiftID;
+    const q = {};
+    q.sql = 'SELECT ?? FROM ?? WHERE ?? = ? AND ?? = ?';
+    q.values = ['review', 'ShiftReviewOnUser', 'fk_ShiftReviewOnUser_userID', userID, 'fk_ShiftReviewOnUser_shiftID', shiftID];
+    this.body = yield query(q);
+  })
+
+  .get('/avg/user/:userID', function* getAvgUserReview() {
+    const userID = this.params.userID;
+    const q = {};
+    q.sql = 'SELECT AVG(??) FROM ?? WHERE ?? = ?';
+    q.values = ['review', 'ShiftReviewOnUser', 'fk_ShiftReviewOnUser_userID', userID];
+    this.body = yield query(q);
+  })
+
+  .delete('/user/:userID/shift/:shiftID', function* deleteUserReview() {
+    const userID = this.params.userID;
+    const shiftID = this.params.shiftID;
+    const q = {};
+    q.sql = 'DELETE FROM ?? WHERE ?? = ? AND ?? = ?';
+    q.values = ['ShiftReviewOnUser', 'fk_ShiftReviewOnUser_userID', userID, 'fk_ShiftReviewOnUser_shiftID', shiftID];
+    this.body = yield query(q);
+  })
+
+  .post('/facility/:facilityID/shift/:shiftID', function* createFacilityReview() {
+    const facilityID = this.params.facilityID;
+    const shiftID = this.params.shiftID;
+    const requestJson = this.request.body.fields;
+    requestJson.fk_ShiftReviewOnFacility_facilityID = facilityID;
+    requestJson.fk_ShiftReviewOnFacility_shiftID = shiftID;
+    const q = {};
+    q.sql = 'INSERT INTO ?? SET ?';
+    q.values = ['ShiftReviewOnFacility', requestJson];
+    this.body = yield query(q);
+  })
+
+  .put('/facility/:facilityID/shift/:shiftID', function* updateFacilityReview() {
+    const facilityID = this.params.facilityID;
+    const shiftID = this.params.shiftID;
+    const requestJson = this.request.body.fields;
+    const q = {};
+    q.sql = 'UPDATE ?? SET ? WHERE ?? = ? AND ?? = ?';
+    q.values = ['ShiftReviewOnFacility', requestJson, 'fk_ShiftReviewOnFacility_facilityID', facilityID, 'fk_ShiftReviewOnFacility_shiftID', shiftID];
+    this.body = yield query(q);
+  })
+
+  .get('/facility/:facilityID/shift/:shiftID', function* getFacilityReview() {
+    const facilityID = this.params.facilityID;
+    const shiftID = this.params.shiftID;
+    const q = {};
+    q.sql = 'SELECT ?? FROM ?? WHERE ?? = ? AND ?? = ?';
+    q.values = ['review', 'ShiftReviewOnFacility', 'fk_ShiftReviewOnFacility_facilityID', facilityID, 'fk_ShiftReviewOnFacility_shiftID', shiftID];
+    this.body = yield query(q);
+  })
+
+  .get('/avg/facility/:facilityID', function* getAvgFacilityReview() {
+    const facilityID = this.params.facilityID;
+    const q = {};
+    q.sql = 'SELECT AVG(??) FROM ?? WHERE ?? = ?';
+    q.values = ['review', 'ShiftReviewOnFacility', 'fk_ShiftReviewOnFacility_facilityID', facilityID];
+    this.body = yield query(q);
+  })
+
+  .delete('/facility/:facilityID/shift/:shiftID', function* deleteFacilityReview() {
+    const facilityID = this.params.facilityID;
+    const shiftID = this.params.shiftID;
+    const q = {};
+    q.sql = 'DELETE FROM ?? WHERE ?? = ? AND ?? = ?';
+    q.values = ['ShiftReviewOnFacility', 'fk_ShiftReviewOnFacility_facilityID', facilityID, 'fk_ShiftReviewOnFacility_shiftID', shiftID];
+    this.body = yield query(q);
+  });
+
+  app.use(shiftReview.routes())
+    .use(shiftReview.allowedMethods());
+};
--- server/routes/shiftRoutes.js
+++ server/routes/shiftRoutes.js
@@ -4,7 +4,7 @@
 module.exports = function(app) {
   require('./shift')(app);
   require('./shiftStatus')(app);
-  // require('./userEmail')(app);
+  require('./shiftReviews')(app);
   // require('./userLicense')(app);
   // require('./userPhone')(app);
   // require('./userSchedule')(app);

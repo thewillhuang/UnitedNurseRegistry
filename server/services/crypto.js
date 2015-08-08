bb5d05536fe22c0d889cf7b981dd67edb92962a0
William Huang <will.h86@gmail.com>
2015-08-07 19:38:16 -0700

    working... crypto

--- server/services/crypto.js
+++ server/services/crypto.js
@@ -1,39 +1,30 @@
 'use strict';
 
+// Nodejs encryption with CTR
 const aes = module.exports = {};
-const forge = require('node-forge');
-const fs = require('fs');
-const cert = fs.readFileSync('../../aes-key.pem');
-const alg = 'AES-CTR';
-const salt = forge.random.getBytesSync(128);
-const key = forge.pkcs5.pbkdf2(cert, salt, 5, 16);
+const crypto = require('crypto');
+const algorithm = 'AES-256-CTR';
+const key = crypto.randomBytes(32);
 
 aes.encrypt = function(text) {
-  console.log(alg, key);
-  const iv = forge.random.getBytesSync(16);
-  const cipher = forge.cipher.createCipher(alg, key);
-  cipher.start({iv: iv});
-  cipher.update(forge.util.createBuffer(text, 'utf8'));
-  cipher.finish();
-  const encrypted = cipher.output;
-  const crypt = encrypted.toHex();
-  const ivhex = forge.util.bytesToHex(iv);
-  return {crypt, ivhex};
+  const iv = new Buffer(crypto.randomBytes(16));
+  console.log('iv', iv);
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  let crypted = cipher.update(text, 'utf8', 'hex');
+  crypted += cipher.final('hex');
+  return {payload: crypted, iv: iv.toString('hex')};
 };
 
 aes.decrypt = function(encrypted, iv) {
-  console.log(alg, key);
-  const ivbyte = forge.util.hexToBytes(iv);
-  const decipher = forge.cipher.createDecipher(alg, key);
-  decipher.start({iv: ivbyte});
-  decipher.update(encrypted);
-  decipher.finish();
-  // outputs decrypted hex
-  const decrypted = decipher.output;
-  const dec = decrypted.toHex();
+  const ivbuf = new Buffer(iv, 'hex');
+  console.log('ivbuf', ivbuf);
+  const decipher = crypto.createDecipheriv(algorithm, key, ivbuf);
+  let dec = decipher.update(encrypted, 'hex', 'utf8');
+  dec += decipher.final('utf8');
   return dec;
 };
 
-const encrypted2 = aes.encrypt('testing 1');
-console.log(encrypted2);
-console.log(aes.decrypt(encrypted2.crypt, encrypted2.ivhex));
+const hw = aes.encrypt('hello world');
+console.log(hw);
+// outputs hello world
+console.log(aes.decrypt(hw.payload, hw.iv));

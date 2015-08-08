352c73009a7aaa516c23ec4a9a410207c65e5589
William Huang <will.h86@gmail.com>
2015-08-07 19:45:17 -0700

    added comments to remember the fucking pain...

--- server/services/crypto.js
+++ server/services/crypto.js
@@ -4,26 +4,30 @@
 const aes = module.exports = {};
 const crypto = require('crypto');
 const algorithm = 'AES-256-CTR';
+// for aes 256, length of key need to be exactically 32 bytes
+// generate a cryptographically strong 32 byte data
 const key = crypto.randomBytes(32);
 
-aes.encrypt = function(text) {
+aes.encrypt = function(payload) {
+  // iv must be a buffer, the length must be 16.
   const iv = new Buffer(crypto.randomBytes(16));
-  console.log('iv', iv);
   const cipher = crypto.createCipheriv(algorithm, key, iv);
-  let crypted = cipher.update(text, 'utf8', 'hex');
+  let crypted = cipher.update(payload, 'utf8', 'hex');
   crypted += cipher.final('hex');
+  // convert iv to hex encoding
   return {payload: crypted, iv: iv.toString('hex')};
 };
 
 aes.decrypt = function(encrypted, iv) {
+  // convert iv from hex coding to a buffer, default is utf-8, will get error wrong length
   const ivbuf = new Buffer(iv, 'hex');
-  console.log('ivbuf', ivbuf);
   const decipher = crypto.createDecipheriv(algorithm, key, ivbuf);
   let dec = decipher.update(encrypted, 'hex', 'utf8');
   dec += decipher.final('utf8');
   return dec;
 };
 
+// normal string is utf-8 encoding
 const hw = aes.encrypt('hello world');
 console.log(hw);
 // outputs hello world

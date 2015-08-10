6418ad7ca370698bb334653894f7b7678c2e9f49
William Huang <will.h86@gmail.com>
2015-08-09 22:12:02 -0700

    updates

--- test/user.js
+++ test/user.js
@@ -97,8 +97,8 @@ describe('user api', function() {
       })
       .expect(200)
       .end(function(err, res) {
-        console.log('body', res.body);
         console.log('headers', res.headers);
+        console.log('body', res.body);
         // expect(res.body).to.be.an('object');
         // expect(res.body.success).to.equal(true);
         // expect(err).to.be.a('null');
@@ -110,8 +110,8 @@ describe('user api', function() {
     request.get('/api/auth/logout')
       .expect(200)
       .end(function(err, res) {
-        console.log('body', res.body);
         console.log('headers', res.headers);
+        console.log('body', res.body);
         // expect(res.body).to.be.an('object');
         // expect(res.body.success).to.equal(true);
         // expect(err).to.be.a('null');

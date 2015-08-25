937c0b39c5e1ee07b3fd21d0bd805d05834e3841
William Huang <will.h86@gmail.com>
2015-08-25 15:36:21 -0700

    added changes

--- src/javascripts/components/forHospitals.jsx
+++ src/javascripts/components/forHospitals.jsx
@@ -17,17 +17,17 @@ class ForHospitals extends React.Component {
           <div href='#' className='grid2-item'>
             <i className='fa icon-paper-plane fa-2x grid2-i'></i>
             <h1>Post</h1>
-            <p>Post a shift and specify your price range</p>
+            <p>Shifts and specify your price range</p>
           </div>
           <div href='#' className='grid2-item'>
             <i className='fa icon-graph fa-2x grid2-i'></i>
             <h1>Real Time</h1>
-            <p>Get real time Feedback from nurses around you. Accept the ones you want.</p>
+            <p>Pricing Feedback from nurses around you. Accept the ones you want.</p>
           </div>
           <div href='#' className='grid2-item'>
             <i className='fa icon-credit-card fa-2x grid2-i'></i>
             <h1>Pay</h1>
-            <p>Pay securely for the nurses's shift</p>
+            <p>Securely for the nurses's shift</p>
           </div>
           <div href='#' className='grid2-omega'>
           </div>

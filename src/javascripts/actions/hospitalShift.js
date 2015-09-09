d8181e6497cb5aaa6855df5047059e3816dbce1d
William Huang <will.h86@gmail.com>
2015-09-09 16:50:21 -0700

    miss a change

--- src/javascripts/actions/hospitalShift.js
+++ src/javascripts/actions/hospitalShift.js
@@ -1,19 +1,31 @@
 import shiftConst from '../constants/shiftConstants.js';
-import geohash from 'ngeohash';
 import Dispatcher from '../dispatcher/appDispatcher.js';
 import Promise from 'bluebird';
 import superagent from 'superagent';
 const request = Promise.promisifyAll(superagent);
-
+const token = { Authorization: sessionStorage.getItem('token') };
 const hospitalShift = {};
-
 
 hospitalShift.grabAllActive = (facilityID) => {
   request
     .get('/api/shift/active/' + facilityID)
-    .set({ Authorization: sessionStorage.getItem('token') })
+    .set(token)
     .endAsync().then(function(res) {
-      console.log(res.body);
+      console.log('grabAllActive', res.body);
+    });
+};
+
+hospitalShift.postShift = (payload) => {
+  // first grab a specialty id given a string
+  request
+    .post('');
+
+  // then post a shift with the specialty id
+  request
+    .post('/api/shift/facility/' + payload.facilityID)
+    .set(token)
+    .endAsync().then(function(res) {
+      console.log('postShifts', res.body);
     });
 };
 

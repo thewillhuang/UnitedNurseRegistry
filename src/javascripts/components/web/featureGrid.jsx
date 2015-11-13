5896303303b1651b95929856584f9d81477f2478
William Huang <will.h86@gmail.com>
2015-11-13 14:46:16 -0800

    change icon to a star

--- src/javascripts/components/web/featureGrid.jsx
+++ src/javascripts/components/web/featureGrid.jsx
@@ -15,7 +15,7 @@ export default class FeatureGrid extends React.Component {
 
         <div className='grid-items-lines'>
           <div href='#' className='grid-item'>
-            <i className='fa icon-speedometer fa-2x grid-i'></i>
+            <i className='fa icon-star fa-2x grid-i'></i>
             <h1>Fast Matching</h1>
             <p>Find a Match with your criteria in seconds.</p>
           </div>

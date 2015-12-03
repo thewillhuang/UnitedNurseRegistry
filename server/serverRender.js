5043eb0bc7690dcb91f670fa88a06209afc2964d
william <will.h86@gmail.com>
2015-12-02 18:20:56 -0800

    memoized serverRender for different userAgent

--- server/serverRender.js
+++ server/serverRender.js
@@ -6,14 +6,12 @@ import { match, RoutingContext } from 'react-router';
 import routes from '../src/javascripts/routes';
 import React from 'react';
 import etag from 'etag';
+const html = {};
+
 module.exports = function reactRender(app) {
-  let html;
   serverRender
   // get iso
   .get('/', function* render() {
-    global.navigator = {
-      userAgent: this.request.headers['user-agent'],
-    };
     match({ routes, location: this.path }, (error, redirectLocation, renderProps) => {
       if (error) {
         this.status = 500;
@@ -21,26 +19,34 @@ module.exports = function reactRender(app) {
       } else if (redirectLocation) {
         this.redirect(redirectLocation.pathname + redirectLocation.search);
       } else if (renderProps) {
-        html = html || renderToStaticMarkup(
-          <html lang='en'>
-          <head>
-            <meta charSet='UTF-8' />
-            <meta name='viewport' content='width=device-width, initial-scale=1' />
-            <title>United Nurse Registery</title>
-            <link rel='stylesheet' href='/stylesheets/home.css' />
-            <link rel='shortcut icon' type='image/x-icon' href='/images/favicon.ico' />
-          </head>
-          <body>
-            <div id='root'>
-              <RoutingContext {...renderProps} />
-            </div>
-            <script src='/javascripts/index.js' async />
-            <script src='https://checkout.stripe.com/checkout.js' async />
-          </body>
-          </html>
-        );
-        this.set('ETag', etag(html));
-        this.body = html;
+        const userAgent = this.request.headers['user-agent'];
+        global.navigator = { userAgent };
+        const serveMemoizedHtml = (agent) => {
+          if (html[agent]) {
+            return html[userAgent];
+          }
+          html[userAgent] = renderToStaticMarkup(
+            <html lang='en'>
+            <head>
+              <meta charSet='UTF-8' />
+              <meta name='viewport' content='width=device-width, initial-scale=1' />
+              <title>United Nurse Registery</title>
+              <link rel='stylesheet' href='/stylesheets/home.css' />
+              <link rel='shortcut icon' type='image/x-icon' href='/images/favicon.ico' />
+            </head>
+            <body>
+              <div id='root'>
+                <RoutingContext {...renderProps} />
+              </div>
+              <script src='/javascripts/index.js' async />
+              <script src='https://checkout.stripe.com/checkout.js' async />
+            </body>
+            </html>
+          );
+          return html[agent];
+        };
+        this.set('ETag', etag(serveMemoizedHtml(userAgent)));
+        this.body = serveMemoizedHtml(userAgent);
       } else {
         this.status = 404;
         this.body = 'Not Found';

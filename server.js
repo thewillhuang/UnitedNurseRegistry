e96a0bb1340fb6f628aae44b428af1ac87222993
William <will.h86@gmail.com>
2015-12-22 16:07:16 -0800

    wow cors man wow

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(compress());
 app.use(function* (next) {
   yield next;
   this.set({
-    'Access-Control-Allow-Origin': 'www.unitednurseregistry.com',
+    'Access-Control-Allow-Origin': 'http://www.unitednurseregistry.com',
     'Access-Control-Allow-Methods': 'POST, GET, PUT, OPTIONS, DELETE',
     'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
   });

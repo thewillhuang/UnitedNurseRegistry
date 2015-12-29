5e84e526fea27d1c519dba086fef06c8942f5acf
William <will.h86@gmail.com>
2015-12-28 20:44:34 -0800

    purifycss don't work :()

---
+++ gulpfile.js/tasks/purifycss.js
@@ -0,0 +1,19 @@
+// 'use strict';
+//
+// const gulp = require('gulp');
+// const browserSync = require('browser-sync');
+// const root = require('../config/index');
+// const purify = require('gulp-purifycss');
+// // console.log(root.publicDirectory + '**/*.{jsx,js,html}');
+// const config = require('../config/sass');
+//
+// gulp.task('purify', function () {
+//   return gulp.src(root.publicDirectory + '/**/*.css')
+//     .pipe(purify([
+//       root.sourceDirectory + '**/*.jsx',
+//       root.sourceDirectory + '**/*.js',
+//       root.sourceDirectory + '**/*.html',
+//     ], { info: true, rejected: true }))
+//     .pipe(gulp.dest(root.publicDirectory))
+//     .pipe(browserSync.reload({ stream: true }));
+// });

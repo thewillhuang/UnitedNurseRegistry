7ab097c4cc104743d559cee6eb5aa87923cea5da
William <will.h86@gmail.com>
2016-02-07 15:59:09 -0800

    publish script

--- server/routes/shiftRoutes.js
+++ server/routes/shiftRoutes.js
@@ -5,9 +5,4 @@ module.exports = function (app) {
   require('./shift')(app);
   require('./shiftStatus')(app);
   require('./shiftReviews')(app);
-  // require('./userLicense')(app);
-  // require('./userPhone')(app);
-  // require('./userSchedule')(app);
-  // require('./userSpecialty')(app);
-  // require('./userWorkHistory')(app);
 };
